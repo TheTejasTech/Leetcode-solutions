a31f6174fa4da190afd68ec4a62e395e6a02a126
Tejas Kamble <kambletc@gmail.com>
2025-03-04 19:22:46 +0530

    Time: 0 ms (100%), Space: 7.9 MB (19.48%) - LeetHub

--- 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.cpp
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
-    bool solve(int n,int x){
-        if(n==0){
-            return true;
-        }
-        while(x<=n){
-            if(solve(n-x,x*3)==true){
-                return true;
-            }
-            x=x*3;
-        }
-        return false;
-    }
+    
     bool checkPowersOfThree(int n) {
-        int x=1;
-        return solve(n,x);
+        while (n > 0) {
+            if (n % 3 == 2) {
+                return false;
+            }
+            n /= 3;
+        }
+        return true;
+
     }
 };
