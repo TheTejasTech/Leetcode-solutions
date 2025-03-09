c519dfb12b082b20bc9eb8f7ddee82d7e4c4596e
Tejas Kamble <kambletc@gmail.com>
2025-03-09 22:35:54 +0530

    Time: 2 ms (98.04%), Space: 63.5 MB (19.27%) - LeetHub

--- 3483-alternating-groups-ii/3483-alternating-groups-ii.java
+++ 3483-alternating-groups-ii/3483-alternating-groups-ii.java
@@ -1,15 +1,18 @@
 class Solution {
     public int numberOfAlternatingGroups(int[] colors, int k) {
-        int n = colors.length;
-        int ans = 0, cnt = 0;
-        for (int i = 0; i < n << 1; ++i) {
-            if (i > 0 && colors[i % n] == colors[(i - 1) % n]) {
+        int cnt = 1;
+        int res = 0;
+        int firstcnt = 0;
+        for (int i = 1; i < colors.length; i++) {
+            if (colors[i] != colors[i-1]) cnt++;
+            else {
+                if (firstcnt == 0) firstcnt = Math.min(k-1, cnt); 
                 cnt = 1;
-            } else {
-                ++cnt;
             }
-            ans += i >= n && cnt >= k ? 1 : 0;
+            if (cnt >= k) res++;
         }
-        return ans;
+        if (colors[colors.length-1] == colors[0]) cnt = -1000;
+        if (cnt == colors.length) return cnt;
+        return res + Math.max(0, firstcnt + Math.min(cnt, k-1) - k + 1);
     }
 }
