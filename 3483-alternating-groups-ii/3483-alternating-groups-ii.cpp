47072583832cfa9b1885923f1a32e19e3d1e7cfc
Tejas Kamble <kambletc@gmail.com>
2025-03-09 22:38:07 +0530

    Time: 37 ms (100%), Space: 102.6 MB (91.84%) - LeetHub

--- 3483-alternating-groups-ii/3483-alternating-groups-ii.cpp
+++ 3483-alternating-groups-ii/3483-alternating-groups-ii.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
-    int numberOfAlternatingGroups(vector<int>& colors, int k) {
-        int n = colors.size();
-        int ans = 0, cnt = 0;
-        for (int i = 0; i < n << 1; ++i) {
-            if (i && colors[i % n] == colors[(i - 1) % n]) {
-                cnt = 1;
-            } else {
-                ++cnt;
-            }
-            ans += i >= n && cnt >= k ? 1 : 0;
+    static int numberOfAlternatingGroups(vector<int>& colors, int k) {
+        const int n=colors.size(), sz=n+k-1;
+        int ans=0, alt=1, prev=colors[0];
+        for(int i=1; i<sz; i++){
+            alt=(prev==colors[i%n])?1:alt+1;
+            if (alt>=k) ans++;
+            prev=colors[i%n];
         }
-        return ans;
+        return  ans;
     }
 };
+
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
