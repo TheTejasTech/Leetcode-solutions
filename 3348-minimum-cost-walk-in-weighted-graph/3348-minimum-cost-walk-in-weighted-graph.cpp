0be02a8814f49d274753ca64fac08fd40a49bafc
Tejas Kamble <kambletc@gmail.com>
2025-03-20 09:20:25 +0530

    Time: 4 ms (100%), Space: 203.4 MB (98.05%) - LeetHub

--- 3348-minimum-cost-walk-in-weighted-graph/3348-minimum-cost-walk-in-weighted-graph.cpp
+++ 3348-minimum-cost-walk-in-weighted-graph/3348-minimum-cost-walk-in-weighted-graph.cpp
@@ -1,58 +1,53 @@
-struct DSU {
-    vector<int> p, sz;
-    
-    DSU(int n) {
-        p.resize(n+1);
-        sz.resize(n+1, 1);
-        
-        for (int j = 0; j <= n; j ++) p[j] = j;
-    }
-    
-    int Leader (int x) {
-        if (x == p[x]) return x;
-        return (p[x] = Leader(p[x]));
-    }
-    
-    void Merge (int x, int y) {
-        x = Leader(x);
-        y = Leader(y);
-        
-        if (x == y) return;
-        if (sz[x] > sz[y]) swap(x, y);
-        
-        sz[y] += sz[x];
-        p[x] = y;
-    }
+static auto init = []() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  return nullptr;
+}();
+
+constexpr unsigned int max_weight = (1 << bit_width(100'000u)) - 1u;
+
+struct DisjointSet {
+  DisjointSet* parent = this;
+  unsigned int weight = max_weight;
 };
+
+DisjointSet& find(DisjointSet& x) {
+  auto px = &x;
+  while (px != px->parent) {
+    tie(px, px->parent) = make_tuple(px->parent, px->parent->parent);
+  }
+  return *px;
+}
+
+void unify(DisjointSet& x, DisjointSet& y, unsigned int weight) {
+  auto px = &find(x);
+  auto py = &find(y);
+  if (px == py) {
+    if (px->weight) px->weight &= weight;
+    return;
+  }
+  if (py->weight < px->weight) swap(px, py);
+  if (px->weight) px->weight &= py->weight & weight;
+  py->parent = px;
+}
 
 class Solution {
 public:
-    vector<int> minimumCost(int n, vector<vector<int>>& edges, vector<vector<int>>& query) {
-        DSU dsu(n);
-        for (auto e : edges) dsu.Merge (e[0], e[1]);
-            
-        vector<int> leader_val(n, -1);
-        for (auto e : edges) {
-            int x = dsu.Leader(e[0]);
-            int y = dsu.Leader(e[1]);            
-            assert (x == y);
-            
-            if (leader_val[x] == -1) leader_val[x] = e[2];
-            else leader_val[x] &= e[2];
-        }
-                
-        vector<int> ans;
-        for (auto q : query) {
-            if (q[0] == q[1]) {
-                ans.push_back(0);
-                continue;
-            }
-            int x = dsu.Leader(q[0]);
-            int y = dsu.Leader(q[1]);
-            
-            if (x != y) ans.push_back(-1);
-            else ans.push_back(leader_val[x]);            
-        }
-        return ans;
+  vector<int> minimumCost(int n, const vector<vector<int>>& edges, const vector<vector<int>>& query) {
+    auto forest = vector<DisjointSet>(n);
+    for (auto& edge : edges) {
+      auto [u, v, w] = make_tuple(edge[0], edge[1], edge[2]);
+      unify(find(forest[u]), find(forest[v]), w);
     }
+
+    auto ans = vector<int>{};
+    ans.reserve(size(query));
+    for (auto& q : query) {
+      auto [u, v] = make_pair(q[0], q[1]);
+      const auto& node_u = find(forest[u]);
+      const auto& node_v = find(forest[v]);
+      ans.push_back(&node_u == &node_v ? node_u.weight : -1);
+    }
+    return ans;
+  }
 };
