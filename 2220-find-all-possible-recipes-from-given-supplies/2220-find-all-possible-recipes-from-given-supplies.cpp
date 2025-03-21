1e1c991e49fb28c259a73b8d27e1b48eba08eaf0
Tejas Kamble <kambletc@gmail.com>
2025-03-21 08:52:30 +0530

    Time: 15 ms (99.9%), Space: 99.8 MB (94.57%) - LeetHub

--- 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.cpp
+++ 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.cpp
@@ -1,31 +1,78 @@
+//#define TRACE
+
 class Solution {
 public:
-    vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
-        unordered_map<string, vector<string>> g;
-        unordered_map<string, int> indeg;
-        for (int i = 0; i < recipes.size(); ++i) {
-            for (auto& v : ingredients[i]) {
-                g[v].push_back(recipes[i]);
-            }
-            indeg[recipes[i]] = ingredients[i].size();
-        }
-        queue<string> q;
-        for (auto& s : supplies) {
-            q.push(s);
-        }
-        vector<string> ans;
-        while (!q.empty()) {
-            for (int n = q.size(); n; --n) {
-                auto i = q.front();
-                q.pop();
-                for (auto j : g[i]) {
-                    if (--indeg[j] == 0) {
-                        ans.push_back(j);
-                        q.push(j);
-                    }
+
+    enum class Available
+    {
+        Yes,
+        No,
+        Unknown
+    };
+
+    bool canMakeRecipe(int recipeIdx, vector<string>& recipes, vector<vector<string>>& ingredients, unordered_map<string, int>& supplies, vector<Available>& availableRecipes, unordered_map<string, int>& recipesMap)
+    {
+        if (availableRecipes[recipeIdx] != Available::Unknown)
+            return availableRecipes[recipeIdx] == Available::Yes ? true : false;
+
+        // Assume no, in case there is a loop
+        availableRecipes[recipeIdx] = Available::No;
+
+        #ifdef TRACE
+            cout << "Testing " << recipeIdx << " (" << recipes[recipeIdx] << ")" << endl;
+        #endif
+
+        for (const string& ingredient : ingredients[recipeIdx])
+        {
+            if (supplies.find(ingredient) == supplies.end())
+            {
+                auto recipe = recipesMap.find(ingredient);
+                if (recipe == recipesMap.end())
+                {
+                    availableRecipes[recipeIdx] = Available::No;
+                    return false;
+                }
+
+                const int idx = recipe->second;
+                #ifdef TRACE
+                    cout << " - Needs " << idx << endl;
+                #endif
+                if (!canMakeRecipe(idx, recipes, ingredients, supplies, availableRecipes, recipesMap))
+                {
+                    availableRecipes[recipeIdx] = Available::No;
+                    return false;
                 }
             }
         }
-        return ans;
+
+        availableRecipes[recipeIdx] = Available::Yes;
+        return true;
+    }
+
+    vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies)
+    {
+        vector<string> available;
+
+        unordered_map<string, int> suppliesMap;
+        for (const string& supply : supplies)
+        {
+            suppliesMap[supply] = 1;
+        }
+
+        unordered_map<string, int> recipesMap;
+        for (int i=0; i<recipes.size(); ++i)
+        {
+            recipesMap[recipes[i]] = i;
+        }
+
+        vector<Available> availableRecipes(recipes.size(), Available::Unknown);
+
+        for (int i=0; i<recipes.size(); ++i)
+        {
+            if (canMakeRecipe(i, recipes, ingredients, suppliesMap, availableRecipes, recipesMap))
+                available.push_back(recipes[i]);
+        }
+
+        return available;
     }
 };
