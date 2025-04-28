10cf5c923a62f149b492f116b62240c7989fc6af
Tejas Kamble <kambletc@gmail.com>
2025-04-28 17:46:41 +0530

    Time: 2 ms (100%), Space: 60.3 MB (91.24%) - LeetHub

--- 2394-count-subarrays-with-score-less-than-k/2394-count-subarrays-with-score-less-than-k.java
+++ 2394-count-subarrays-with-score-less-than-k/2394-count-subarrays-with-score-less-than-k.java
@@ -1,23 +1,20 @@
 class Solution {
     public long countSubarrays(int[] nums, long k) {
-        int n = nums.length;
-        long[] s = new long[n + 1];
-        for (int i = 0; i < n; ++i) {
-            s[i + 1] = s[i] + nums[i];
-        }
-        long ans = 0;
-        for (int i = 1; i <= n; ++i) {
-            int l = 0, r = i;
-            while (l < r) {
-                int mid = (l + r + 1) >> 1;
-                if ((s[i] - s[i - mid]) * mid < k) {
-                    l = mid;
-                } else {
-                    r = mid - 1;
-                }
+        
+        long total =0 ;
+        long count=0;
+        for (int start=0,end =0 ; end <nums.length;end++){
+
+            total += nums[end];
+
+            while (start<=end && total*(end-start+1)>=k){
+                total -=nums[start];
+                start++;
             }
-            ans += l;
+            count += end -start+1;
+
         }
-        return ans;
+        return count;
+
     }
 }
