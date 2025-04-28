8d82978f4ec31cd5ce4059c1794ce9b10a9bc985
Tejas Kamble <kambletc@gmail.com>
2025-04-28 17:47:38 +0530

    Time: 0 ms (100%), Space: 99 MB (76.24%) - LeetHub

--- 2394-count-subarrays-with-score-less-than-k/2394-count-subarrays-with-score-less-than-k.cpp
+++ 2394-count-subarrays-with-score-less-than-k/2394-count-subarrays-with-score-less-than-k.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     long long countSubarrays(vector<int>& nums, long long k) {
-        int n = nums.size();
-        long long s[n + 1];
-        s[0] = 0;
-        for (int i = 0; i < n; ++i) {
-            s[i + 1] = s[i] + nums[i];
-        }
-        long long ans = 0;
-        for (int i = 1; i <= n; ++i) {
-            int l = 0, r = i;
-            while (l < r) {
-                int mid = (l + r + 1) >> 1;
-                if ((s[i] - s[i - mid]) * mid < k) {
-                    l = mid;
-                } else {
-                    r = mid - 1;
-                }
+        long long res = 0;
+        long long sum = 0;
+        long long count = 0;
+        int low = 0;
+        for(int i=0; i<nums.size(); i++){
+            sum += nums[i];
+            count++;
+            while(sum*count >= k){
+                sum -= nums[low];
+                count--;
+                low++;
             }
-            ans += l;
+            res += count;
         }
-        return ans;
+        return res;
     }
 };
