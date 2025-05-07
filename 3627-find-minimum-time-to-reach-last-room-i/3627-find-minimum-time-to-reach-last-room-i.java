7da7009184189610f7a95086f290a61a00a0dd4a
Tejas Kamble <kambletc@gmail.com>
2025-05-07 22:07:08 +0530

    Time: 4 ms (100%), Space: 45 MB (53.27%) - LeetHub

--- 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.java
+++ 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.java
@@ -1,52 +1,94 @@
 class Solution {
-    private static final int[] dir = {-1, 0, 1, 0, -1};
+	private static class Room implements Comparable<Room> {
+		final int openTime;
+		Room[] adjacent;
+		Room next;
 
-    static class Node {
-        int r, c, time;
+		Room() {
+			openTime = Integer.MAX_VALUE;
+		}
 
-        Node(int r, int c, int time) {
-            this.r = r;
-            this.c = c;
-            this.time = time;
-        }
-    }
+		Room(int openTime) {
+			this.openTime = openTime;
+			next = this; // indicates that this Room hasn't been approached yet
+		}
 
-    static class Comparator implements java.util.Comparator<Node> {
-        public int compare(Node p1, Node p2) {
-            return Integer.compare(p1.time, p2.time);
-        }
-    }
+		@Override
+		public int compareTo(Room other) {
+			return openTime - other.openTime;
+		}
+	}
 
-    private boolean isValid(int r, int c, int m, int n) {
-        return (r >= 0 && r < m && c >= 0 && c < n);
-    }
+	private static final Room DUMMY_ROOM = new Room();
 
-    public int minTimeToReach(int[][] moveTime) {
-        int m = moveTime.length;
-        int n = moveTime[0].length;
+	private static Room initRooms(int[][] moveTime) {
+		int n = moveTime.length;
+		int m = moveTime[0].length;
+		Room[][] rooms = new Room[n][m];
+		for (int i = 0; i < n; i++) {
+			int[] mtRow = moveTime[i];
+			Room[] rRow = rooms[i];
+			for (int j = 0; j < m; j++)
+				rRow[j] = new Room(mtRow[j]);
+		}
+		Room[] dummyRow = new Room[m];
+		Arrays.fill(dummyRow, DUMMY_ROOM);
+		Room[] prevRow = dummyRow;
+		Room[] curRow = rooms[0];
+		n--;
+		m--;
+		for (int i = 0; i <= n; i++) {
+			Room[] nextRow = i < n ? rooms[i + 1] : dummyRow;
+			Room prev = DUMMY_ROOM;
+			Room cur = curRow[0];
+			for (int j = 0; j <= m; j++) {
+				Room next = j < m ? curRow[j + 1] : DUMMY_ROOM;
+				cur.adjacent = new Room[] { prev, prevRow[j], next, nextRow[j] };
+				prev = cur;
+				cur = next;
+			}
+			prevRow = curRow;
+			curRow = nextRow;
+		}
+		Room start = rooms[0][0];
+		start.next = rooms[n][m]; // finish
+		return start;
+	}
 
-        PriorityQueue<Node> minHeap = new PriorityQueue<>(new Comparator());
-        minHeap.offer(new Node(0, 0, 0));
-        boolean[][] visited = new boolean[m][n];
-        visited[0][0] = true;
-
-        while (!minHeap.isEmpty()) {
-            Node curr = minHeap.poll();
-
-            if (curr.r == m - 1 && curr.c == n - 1)
-                return curr.time;
-
-            for (int i = 0; i < 4; ++i) {
-                int newR = curr.r + dir[i];
-                int newC = curr.c + dir[i + 1];
-
-                if (isValid(newR, newC, m, n) && !visited[newR][newC]) {
-                    Node newNode = new Node(newR, newC, 1 + Math.max(curr.time, moveTime[newR][newC]));
-                    minHeap.offer(newNode);
-                    visited[newR][newC] = true;
-                }
-            }
-        }
-        return -1;
-    }
+	public static int minTimeToReach(int[][] moveTime) {
+		Room start = initRooms(moveTime);
+		Room finish = start.next;
+		Queue<Room> waitingToEnter = new PriorityQueue<>();
+		waitingToEnter.add(DUMMY_ROOM); // guard Room to prevent NPE
+		start.next = null;
+		Room exitingHead = start;
+		int currentTime = 0;
+		while (true) {
+			Room exitingHeadNew = null;
+			while (exitingHead != null) {
+				for (Room adj : exitingHead.adjacent)
+					if (adj.next == adj) {
+						if (adj == finish)
+							return Math.max(currentTime, finish.openTime) + 1;
+						if (adj.openTime <= currentTime) {
+							adj.next = exitingHeadNew;
+							exitingHeadNew = adj;
+						} else {
+							adj.next = null;
+							waitingToEnter.offer(adj);
+						}
+					}
+				exitingHead = exitingHead.next;
+			}
+			exitingHead = exitingHeadNew;
+			int queueTime;
+			while ((queueTime = waitingToEnter.peek().openTime) <= currentTime) {
+				Room entering = waitingToEnter.poll();
+				entering.next = exitingHead;
+				exitingHead = entering;
+			}
+			if (++currentTime < queueTime && exitingHead == null)
+				currentTime = queueTime;
+		}
+	}
 }
