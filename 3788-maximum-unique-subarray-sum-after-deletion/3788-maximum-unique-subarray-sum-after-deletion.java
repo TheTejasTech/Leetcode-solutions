0f9eadd9842108e6bcaf09b63dfe45de3775cf8a
Tejas Kamble <kambletc@gmail.com>
2025-07-25 09:57:17 +0530

    Time: 2 ms (74.06%), Space: 43.2 MB (5.31%) - LeetHub

---
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int maxSum(int[] nums) {
+        // int[] unique=Arrays.stream(nums).distinct().toArray();
+        // int maxs=0;
+        
+        // if()
+        // for(int i=0;i<unique.length;i++){
+        //     maxs+=unique[i];
+        // }
+        // return maxs;
+
+
+        Set<Integer> uniqueElements = new HashSet<>();
+        int result = 0;
+        int maxNegativeValue = Integer.MIN_VALUE;
+
+        for (int num : nums) {
+            if (num > 0) {
+                uniqueElements.add(num);
+            } else {
+                maxNegativeValue = Math.max(maxNegativeValue, num);
+            }
+        }
+
+        for (int num : uniqueElements) {
+            result += num;
+        }
+
+        return result == 0 ? maxNegativeValue : result;
+    }
+}
