4c0cdc4e07402fbab1c369186adb7577d98bcaa6
Tejas Kamble <kambletc@gmail.com>
2025-08-30 22:24:40 +0530

    Time: 14 ms (14.95%), Space: 45.1 MB (11.32%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,24 +1,19 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        boolean[][] row = new boolean[9][9];
-        boolean[][] col = new boolean[9][9];
-        boolean[][] sub = new boolean[9][9];
-        for (int i = 0; i < 9; ++i) {
-            for (int j = 0; j < 9; ++j) {
-                char c = board[i][j];
-                if (c == '.') {
-                    continue;
-                }
-                int num = c - '0' - 1;
-                int k = i / 3 * 3 + j / 3;
-                if (row[i][num] || col[j][num] || sub[k][num]) {
-                    return false;
-                }
-                row[i][num] = true;
-                col[j][num] = true;
-                sub[k][num] = true;
-            }
-        }
-        return true;
+        Set<String> seen = new HashSet<>();
+
+    for (int i = 0; i < 9; ++i)
+      for (int j = 0; j < 9; ++j) {
+        if (board[i][j] == '.')
+          continue;
+        final char c = board[i][j];
+        if (!seen.add(c + "@row" + i) || //
+            !seen.add(c + "@col" + j) || //
+            !seen.add(c + "@box" + i / 3 + j / 3))
+          return false;
+      }
+
+    return true;
+
     }
 }
