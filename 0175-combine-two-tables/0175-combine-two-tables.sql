8f63f720025bb1d89e646018569844a3bc0bdc4e
Tejas Kamble <kambletc@gmail.com>
2025-09-11 16:37:42 +0530

    Time: 399 ms (60.93%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,4 +1,1 @@
-# Write your MySQL query statement below
 select p.firstName , p.lastName , s.city , s.state from Person p left join Address s on p.personId = s.personId;
-
-# Write your MySQL query statement below
