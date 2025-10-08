4cd64853312c75ba045be80cbf603a6b9d15397c
Tejas Kamble <kambletc@gmail.com>
2025-10-08 20:57:01 +0530

    Time: 47 ms (50.53%), Space: 65.1 MB (45.65%) - LeetHub

--- 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.java
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.java
@@ -1,54 +1,53 @@
 class Solution {
     public int[] successfulPairs(int[] spells, int[] potions, long success) {
-        Arrays.sort(potions);
-        int n = spells.length, m = potions.length;
-        int[] result = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            long minPotion = (success + spells[i] - 1) / spells[i]; // Avoid overflow
-            int index = binarySearch(potions, minPotion);
-            result[i] = m - index;
-        }
-
-        return result;
-        // var l = potions.length;
         // Arrays.sort(potions);
-        // var results = new int[spells.length];
+        // int n = spells.length, m = potions.length;
+        // int[] result = new int[n];
 
-        // for (int i=0; i<spells.length; i++) {
-        //     // use bin search to find index of smallest potion that produces success
-        //     var s = spells[i];
-        //     int start = 0, end = l, magicIndex = -1;
-        //     while (start < end) {
-        //         var mid = start + (end - start) / 2;
-        //         long test = (long) s * potions[mid];
-        //         if (test >= success) {
-        //             magicIndex = mid;
-        //             end = mid;
-        //         } else {
-        //             start = mid + 1;
-        //         }
-        //     }
-        //     if (magicIndex != -1) {
-        //         results[i] = l - magicIndex;
-        //     } else {
-        //         results[i] = 0;
-        //     }
+        // for (int i = 0; i < n; i++) {
+        //     long minPotion = (success + spells[i] - 1) / spells[i]; // Avoid overflow
+        //     int index = binarySearch(potions, minPotion);
+        //     result[i] = m - index;
         // }
-        // return results;
-    }
-    private int binarySearch(int[] potions, long minPotion) {
-        int low = 0, high = potions.length - 1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (potions[mid] < minPotion) {
-                low = mid + 1;
+
+        // return result;
+        var l = potions.length;
+        Arrays.sort(potions);
+        var results = new int[spells.length];
+
+        for (int i=0; i<spells.length; i++) { 
+            var s = spells[i];
+            int start = 0, end = l, magicIndex = -1;
+            while (start < end) {
+                var mid = start + (end - start) / 2;
+                long test = (long) s * potions[mid];
+                if (test >= success) {
+                    magicIndex = mid;
+                    end = mid;
+                } else {
+                    start = mid + 1;
+                }
+            }
+            if (magicIndex != -1) {
+                results[i] = l - magicIndex;
             } else {
-                high = mid - 1;
+                results[i] = 0;
             }
         }
-        return low;
+        return results;
     }
+    // private int binarySearch(int[] potions, long minPotion) {
+    //     int low = 0, high = potions.length - 1;
+    //     while (low <= high) {
+    //         int mid = low + (high - low) / 2;
+    //         if (potions[mid] < minPotion) {
+    //             low = mid + 1;
+    //         } else {
+    //             high = mid - 1;
+    //         }
+    //     }
+    //     return low;
+    // }
 
 
 }
