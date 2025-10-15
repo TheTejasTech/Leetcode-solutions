a88c69abfb7e390b3211ecc5ee1b99ab60e0b423
Tejas Kamble <kambletc@gmail.com>
2025-10-15 23:16:12 +0530

    Time: 14 ms (95.74%), Space: 89 MB (34.04%) - LeetHub

--- 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.java
+++ 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.java
@@ -1,23 +1,40 @@
 class Solution {
     public int maxIncreasingSubarrays(List<Integer> nums) {
-        int prev = 0;
-        int curr = 1;
-        int k = 0;
-        int[] a = new int[nums.size()];
-        for(int num=0; num <  nums.size(); num++){
-            a[num] = nums.get(num);
-        }
-        for(int i = 1; i < nums.size(); i++){
-            if(a[i] > a[i-1]){
-                curr++;
-            }else{
-                prev = curr;
-                curr = 1;
-            }
+        // int prev = 0;
+        // int curr = 1;
+        // int k = 0;
+        // int[] a = new int[nums.size()];
+        // for(int num=0; num <  nums.size(); num++){
+        //     a[num] = nums.get(num);
+        // }
+        // for(int i = 1; i < nums.size(); i++){
+        //     if(a[i] > a[i-1]){
+        //         curr++;
+        //     }else{
+        //         prev = curr;
+        //         curr = 1;
+        //     }
 
-            k = Math.max( k , curr/2);
-            k = Math.max(k , Math.min(curr , prev));
+        //     k = Math.max( k , curr/2);
+        //     k = Math.max(k , Math.min(curr , prev));
+        // }
+        // return k;
+        int n = nums.size();
+        int cnt = 1;
+        int precnt = 0;
+        int ans = 0;
+
+        for (int i = 1; i < n; ++i) {
+            if (nums.get(i) > nums.get(i - 1)) {
+                ++cnt;
+            } else {
+                precnt = cnt;
+                cnt = 1;
+            }
+            ans = Math.max(ans, Math.min(precnt, cnt));
+            ans = Math.max(ans, cnt / 2);
         }
-        return k;
+
+        return ans;
     }
 }
