fb4d285636a2f6c70c2018e868263c2e6b84e8aa
Tejas Kamble <kambletc@gmail.com>
2025-11-28 01:02:03 +0530

    Time: 8 ms (77.82%), Space: 171.2 MB (70.65%) - LeetHub

--- 3381-maximum-subarray-sum-with-length-divisible-by-k/3381-maximum-subarray-sum-with-length-divisible-by-k.cpp
+++ 3381-maximum-subarray-sum-with-length-divisible-by-k/3381-maximum-subarray-sum-with-length-divisible-by-k.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     long long maxSubarraySum(vector<int>& nums, int k) {
         int n = nums.size();
-
+    
         vector<long long> prefSum(n);
         prefSum[0] = nums[0];
 
@@ -31,15 +31,6 @@ public:
         }
 
         return result;
-        // int n = nums.size();
-        // long long prefixSum = 0, maxSum = LONG_LONG_MIN;
-        // vector<long long> kSum(k, LONG_LONG_MAX / 2);
-        // kSum[k - 1] = 0;
-        // for (int i = 0; i < n; i++) {
-        //     prefixSum += nums[i];
-        //     maxSum = max(maxSum, prefixSum - kSum[i % k]);
-        //     kSum[i % k] = min(kSum[i % k], prefixSum);
-        // }
-        // return maxSum;
+        
     }
 };
