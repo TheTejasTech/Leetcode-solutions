fd69eea79548641d11a121e5f9cdccd01bcb86d8
Tejas Kamble <kambletc@gmail.com>
2026-01-11 22:44:02 +0530

    Time: 14 ms (76.52%), Space: 50.4 MB (9.98%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.java
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -1,97 +1,53 @@
-import java.util.Stack;
-
 class Solution {
+    
 
-    public int[] getNSR(int[] height ){
-        Stack<Integer> st = new Stack<>();
-        int n = height.length;
-        int[] right = new int[n];
-        int pse = n;
+    private int getLargestAreaInAHistogram(int[] arr, Stack<Integer> st) {
+        st.clear();
 
-        for(int i = n - 1; i >= 0; i--){
-            while(!st.isEmpty() && height[st.peek()] >= height[i]){
-                st.pop();
-            }
+        int max = Integer.MIN_VALUE;
 
-            if(st.isEmpty()){
-                right[i] = pse;
-            }else{
-                right[i] = st.peek();
+        for (int i = 0; i < arr.length; i++) {
+
+            while (!st.isEmpty() && arr[i] < arr[st.peek()]) {
+                int value = arr[st.pop()];
+                int pse = st.isEmpty() ? -1 : st.peek();
+                max = Math.max(max, (i - pse - 1) * value);
             }
 
             st.push(i);
         }
-        return right;
-    }
 
-    public int[] getNSL(int[] height) {
-        Stack<Integer> st = new Stack<>();
-        int n = height.length;
-        int[] left = new int[n];
-        int pse = -1;
-
-        for(int i = 0; i < n; i++){
-            while(!st.isEmpty() && height[st.peek()] >= height[i]){
-                st.pop();
-            }
-
-            if(st.isEmpty()){
-                left[i] = pse;
-            }else{
-                left[i] = st.peek();
-            }
-
-            st.push(i);
-        }
-        return left;
-    }
-
-    public int findArea(int[] height) {
-        int n = height.length;
-        int[] left = getNSL(height);
-        int[] right = getNSR(height);
-        int[] width = new int[n];
-
-        for (int i = 0; i < n; i++){
-            width[i] = right[i] - left[i] - 1;
+        while (!st.isEmpty()) {
+            int value = arr[st.pop()];
+            int pse = st.isEmpty() ? -1 : st.peek();
+            max = Math.max(max, (arr.length - pse - 1) * value);
         }
 
-        int maxarea = 0;
-        for(int i = 0; i < n; i++){
-            int a = width[i] * height[i];
-            if(maxarea < a){
-                maxarea = a;
-            }
-        }
-        return maxarea;
+        return max;
     }
 
     public int maximalRectangle(char[][] matrix) {
-        if(matrix.length == 0) return 0;
+        int maxRectangle = Integer.MIN_VALUE;
 
         int m = matrix.length;
         int n = matrix[0].length;
-        int[] height = new int[n];
 
-        // first row
-        for (int i = 0; i < n; i++) {
-            height[i] = (matrix[0][i] == '1') ? 1 : 0;
-        }
+        Stack<Integer> st = new Stack<>();
 
-        int maxArea = findArea(height);
+        int[] arr = new int[n];
 
-        // remaining rows
-        for (int row = 1; row < m; row++) {
-            for (int col = 0; col < n; col++) {
-                if (matrix[row][col] == '0') {
-                    height[col] = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == '0') {
+                    arr[j] = 0;
                 } else {
-                    height[col] += 1;
+                    arr[j] += 1;
                 }
             }
-            maxArea = Math.max(maxArea, findArea(height));
+
+            maxRectangle = Math.max(maxRectangle, getLargestAreaInAHistogram(arr, st));
         }
 
-        return maxArea;
+        return maxRectangle;
     }
 }
