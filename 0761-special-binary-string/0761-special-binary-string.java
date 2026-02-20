e35ce9f5de23783b82a5ae51ce83857710c45c6d
Tejas Kamble <kambletc@gmail.com>
2026-02-20 15:14:14 +0530

    Time: 2 ms (100%), Space: 43.6 MB (19.67%) - LeetHub

--- 0761-special-binary-string/0761-special-binary-string.java
+++ 0761-special-binary-string/0761-special-binary-string.java
@@ -1,10 +1,9 @@
 class Solution {
     public String makeLargestSpecial(String s) {
-        int sum = 0;
-        int start = 0;
-        List<String> special = new ArrayList<>();
-        for(int i =0; i < s.length();i++){
-            sum += (s.charAt(i) == '1') ? 1: -1;
+        int sum =0, start =0;
+        List<String> special = new ArrayList<>(); 
+        for(int i = 0; i < s.length(); i++){
+            sum += (s.charAt(i)=='1') ? 1:-1;
             if(sum==0){
                 String inner = s.substring(start+1,i);
                 String proceed = "1"+ makeLargestSpecial(inner) + "0";
@@ -12,10 +11,11 @@ class Solution {
                 start = i+1;
             }
         }
+
         Collections.sort(special, Collections.reverseOrder());
         StringBuilder result = new StringBuilder();
-        for(String srt : special){
-            result.append(srt);
+        for(String str: special){
+            result.append(str);
         }
         return result.toString();
     }
