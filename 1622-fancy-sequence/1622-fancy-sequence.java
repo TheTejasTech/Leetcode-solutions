3959129084b26154f0ea36c466f80a6cd42e7534
Tejas Kamble <kambletc@gmail.com>
2026-03-15 19:51:43 +0530

    Time: 60 ms (34.48%), Space: 124.7 MB (50%) - LeetHub

--- 1622-fancy-sequence/1622-fancy-sequence.java
+++ 1622-fancy-sequence/1622-fancy-sequence.java
@@ -1,25 +1,25 @@
 class Fancy {
-    long ma = 1000000007;
     List<Long> seq = new ArrayList<>();
-    long add =0;
     long mul = 1;
+    long add = 0;
+    long ma = 1000000007;
 
     long power(long a, long b){
-        if(b==0){return 1;}
-
+        if(b==0) return 1;
         long half = power(a, b/2);
-        long result = (half * half) %ma;
-        if(b % 2 == 1){
-            result  = (result * a) % ma;
+        long result = (half * half) % ma;
+        if(b%2 == 1){
+            result =( result * a )% ma;
         }
         return result;
     }
+    
     public Fancy() {
-         
+        
     }
     
     public void append(int val) {
-        long x = ((val - add) % ma + ma) * power(mul , ma -2) % ma;
+        long x = ((val - add) % ma +ma) * power(mul , ma -2) % ma;
         seq.add(x);
     }
     
@@ -27,15 +27,14 @@ class Fancy {
         add = (add + inc) % ma;
     }
     
-    public void multAll(int m) { 
-        mul = (m * mul) % ma;
-        add = (m * add) % ma;
+    public void multAll(int m) {
+        mul = (mul * m) % ma;
+        add = (add * m) % ma;
     }
     
     public int getIndex(int idx) {
-        if(idx >= seq.size()){
-            return -1;
-        }
+        if(idx >= seq.size()) return -1;
+
         return (int)((seq.get(idx) * mul + add) % ma);
     }
 }
