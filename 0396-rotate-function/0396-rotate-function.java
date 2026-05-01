463a90983cf1f0701358faa3828e6480b6e493b1
Tejas Kamble <kambletc@gmail.com>
2026-05-01 11:18:05 +0530

    Time: 4 ms (90.35%), Space: 90.3 MB (92.73%) - LeetHub

--- 0396-rotate-function/0396-rotate-function.java
+++ 0396-rotate-function/0396-rotate-function.java
@@ -6,15 +6,13 @@ class Solution {
         int n = nums.length;
         for (int i = 0; i < nums.length; i++) {
             sum += nums[i];
-            f0 += nums[i] * i;
-            //count = Math.max(nums[i], count);
+            f0 += nums[i] * i; 
         }
         int max = f0;
         int curr = f0;
         for (int j = 1; j < nums.length; j++) {
             curr = curr + sum - n * nums[n - j];
-            max = Math.max(curr, max);
-            //tcount = tcount+ count;
+            max = Math.max(curr, max); 
         }
 
         return max;
