2889bc7f6698ef3dd357ead2317c32ec4487f410
Tejas Kamble <kambletc@gmail.com>
2026-05-11 21:02:43 +0530

    Time: 7 ms (28.46%), Space: 46.4 MB (78.62%) - LeetHub

---
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] separateDigits(int[] nums) {
+         List<Integer> list = new ArrayList<>();
+        
+        for (int num : nums) {
+            List<Integer> temp = new ArrayList<>();
+            
+            // Extract digits in reverse
+            while (num > 0) {
+                temp.add(num % 10);
+                num /= 10;
+            }
+            
+            // Add in correct order
+            for (int i = temp.size() - 1; i >= 0; i--) {
+                list.add(temp.get(i));
+            }
+        }
+        
+        // Convert to array
+        int[] res = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            res[i] = list.get(i);
+        }
+        
+        return res;
+    }
+}
