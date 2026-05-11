92da885b668526c8923ac1ec0a1acef2031ff863
Tejas Kamble <kambletc@gmail.com>
2026-05-11 21:07:39 +0530

    Time: 4 ms (47.19%), Space: 16.1 MB (18.38%) - LeetHub

---
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> separateDigits(vector<int>& nums) {
+        vector<int> result;
+        
+        for (auto num : nums) {
+            vector<int> temp;
+            
+            // Extract digits in reverse
+            while (num > 0) {
+                temp.push_back(num % 10);
+                num /= 10;
+            }
+            
+            // Add in correct order
+            for (int i = temp.size() - 1; i >= 0; i--) {
+                result.push_back(temp[i]);
+            }
+        }
+        
+        // Convert to array
+        // int[] res = new int[list.size()];
+        // for (int i = 0; i < list.size(); i++) {
+        //     res[i] = list.get(i);
+        // }
+        
+        return result;
+    }
+};
