c1f9bf5cc58976108c44d289efa6fa5829dc2b35
Tejas Kamble <kambletc@gmail.com>
2026-07-30 14:05:38 +0530

    Time: 0 ms (100%), Space: 42.8 MB (95.69%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int n = word.length();
+        int a = 0;
+        if(n <= 8){
+            a = n;
+        }
+        else if( n >= 8 && n < 16){
+            a = 8 + (( n - 8)*2);
+        }
+        else if( n >= 16 && n < 24){
+            a = 24 + (( n - 16)*3);
+        }
+        else if( n >= 24 && n < 48){
+            a = 48 + (( n - 24)*4);
+        }
+        return a;
+    }
+}
