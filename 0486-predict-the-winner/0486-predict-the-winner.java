5d7ab9a2cd28380b6f376547d80ef929a2c28f53
Tejas Kamble <kambletc@gmail.com>
2026-08-01 13:19:56 +0530

    Time: 76 ms (12.75%), Space: 42.9 MB (41.06%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,25 +1,20 @@
 class Solution {
     public boolean predictTheWinner(int[] nums) {
         int n = nums.length;
-        int total_sum = Arrays.stream(nums).sum();
+        int total_score = Arrays.stream(nums).sum();
 
         int player1 = solve(0, n-1, nums);
-        int player2 = total_sum - player1;
-
-        if(player1 >= player2){
-            return true;
-        }else{
-            return false;
-        }
-        //return false;
+        int player2 = total_score - player1;
+        return player1 >= player2;
     }
 
     public int solve(int i, int j, int[] nums){
-        if(i>j) return 0;
+        if(i > j) return 0;
         if(i == j) return nums[i];
 
-        int takei = nums[i] + Math.min(solve(i+2, j, nums) , solve(i+1,j-1, nums));
+        int takei = nums[i] + Math.min(solve(i+1,j-1, nums) , solve(i+2, j, nums));
         int takej = nums[j] + Math.min(solve(i+1, j-1, nums), solve(i, j-2, nums));
+
         return Math.max(takei, takej);
     }
 }
